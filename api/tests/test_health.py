7c53bd2f234daf05975b560b578c2dbaef9ef448
Alexander Khmelevsky <a.khmelevsky.255@gmail.com>
2021-03-19 19:06:25 +0300

    Fix test

--- api/tests/test_health.py
+++ api/tests/test_health.py
@@ -4,11 +4,11 @@ from rest_framework.status import HTTP_200_OK
 
 # pylint: disable=no-self-use
 class TestHealth(TestCase):
-    databases = "__all__"
+    databases = '__all__'
 
     def test_health_check(self):
         """Test health-check"""
         client = Client()
         response = client \
-            .get(path='/health/', content_type='application/json')
-        assert response.status_code == HTTP_200_OK, 'Check health API'
+            .get(path='/api/v1/health/', content_type='application/json')
+        self.assertEqual(response.status_code, HTTP_200_OK, 'Check health API')
